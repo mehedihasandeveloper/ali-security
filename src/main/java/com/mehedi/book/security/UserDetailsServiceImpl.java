1af19be8c2e0c43260ccda8ad22d3370db3ae8fd
mehedi hasan <mh.ju1803@gmail.com>
2024-11-20 18:16:16 +0600

    1:31

---
+++ src/main/java/com/mehedi/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.mehedi.book.security;
+
+import com.mehedi.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
